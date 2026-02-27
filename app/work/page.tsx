15c39c8584357d15dfca8073205b1ead9b98f5a7
shreyjain11 <mailshreyjain@gmail.com>
2026-02-26 21:05:07 -0600

    add

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -14,7 +14,6 @@ const accolades = [
   { logo: "/verbaAI logo.png", name: "USAJMO Qualifier", url: "https://artofproblemsolving.com/wiki/index.php/United_States_of_America_Junior_Mathematical_Olympiad?srsltid=AfmBOopaIYaUiUCbkhiatRit8sdcV9FvpCslrz3ocZmJOngwFE6gP31l" },
   { logo: "/verbaAI logo.png", name: "Berkeley Math Tournament Guts Round Champion", url: "https://berkeley.mt/" },
   { logo: "/verbaAI logo.png", name: "Valedictorian (1 out of ~500)", url: "https://schools.friscoisd.org/campus/high-school/centennial/home" },
-  { logo: "/MindMeshLogo.png", name: "International Research Olympiad Semifinalist (Top 10% of 4500)", url: "https://www.internationalresearcholympiad.org/" },
   { logo: "/verbaAI logo.png", name: "International Biology Bowl Semifinalist (32 of ~1000)", url: "https://biologybowl.org/" },
   { logo: "/MindMeshLogo.png", name: "Top 12 National Biology Bowl", url: "https://www.nationalbiologybowl.org/" },
   { logo: "/verbaAI logo.png", name: "Provisional Patent Holder"},
